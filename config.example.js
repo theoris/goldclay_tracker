0bfe5c87cedf7ba14b498e9be8a3a025f2165e9d
theoris <karan112@gmail.com>
2025-07-03 12:04:52 +0700

    Create config.example.js

---
+++ config.example.js
@@ -0,0 +1,5 @@
+// config.example.js
+const BOT_TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN';
+const CHAT_ID = '@your_channel_id';
+const APP_ID = 'YOUR_SETTRADE_APP_ID';
+const APP_SECRET = 'YOUR_SETTRADE_APP_SECRET';
